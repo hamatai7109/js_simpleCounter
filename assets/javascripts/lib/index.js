d509e87b035263dfb72b62f27f303acff99d27f7
hamatai <taishihamano0515@gmail.com>
2023-06-18 17:03:40 +0900

    added arrows

--- assets/javascripts/lib/index.js
+++ assets/javascripts/lib/index.js
@@ -3,14 +3,17 @@ let count = 0;
 function countUp(){
   count++;
   document.getElementById('jsi-result').innerHTML = count;
+  document.getElementById('jsi-match').innerHTML += "<li class='match'><img class='matchImg' src='assets/images/common/match.png' alt='マッチ'></li>"
 }
 
 function countDown(){
   count--;
   document.getElementById('jsi-result').innerHTML = count;
+  document.getElementById('jsi-match').lastElementChild.remove();
 }
 
 function reset(){
   count = 0;
   document.getElementById('jsi-result').innerHTML = count;
+  document.getElementById('jsi-match').innerHTML = ``;
 }
